71bbf1792b43326c5cf9bc19b52bd9d7b4885f3a
Alfredo Paes <alfredo.alfpaes@gmail.com>
2021-09-04 08:00:00 -0300

    route to the creation of a user and validation to verify that the same user was created

--- src/index.js
+++ src/index.js
@@ -1,40 +1,58 @@
-const express = require('express');
-const cors = require('cors');
+const { request, response } = require("express");
+const express = require("express");
+const cors = require("cors");
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
-app.post('/users', (request, response) => {
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+
+  const userTodo = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  const userAlreadyExist = users.some((user) => user.username === username);
+
+  if (userAlreadyExist) {
+    return response.status(400).send({ error: "User already exists!" });
+  }
+
+  users.push(userTodo);
+
+  return response.status(201).json(userTodo);
+});
+
+app.get("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
+app.post("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
 
